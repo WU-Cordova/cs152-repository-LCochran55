14a65fc62b58881632370ed43d1c99baf294e4cd
LCochran55 <liamcochran55@gmail.com>
2025-01-24 10:59:51 -0800

    commit

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -4,21 +4,43 @@ from datastructures.ibag import IBag, T
 
 class Bag(IBag[T]): #Bag implements IBAG interface
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
+        self.__bag: dict[T,int] = {}
+
+        if items:
+             for item in items:
+                  self.add(item)
+
         raise NotImplementedError("__init__ method not implemented")
 
     def add(self, item: T) -> None:
+        if item in self.__bag:
+            self.__bag[item] += 1
+        else:
+            self.__bag[item] = 1
         raise NotImplementedError("add method not implemented")
 
     def remove(self, item: T) -> None:
+        if item in self.__bag:
+            self.__bag.pop(item)
+        else:
+            print("Item not in bag")
+            return
         raise NotImplementedError("remove method not implemented")
 
     def count(self, item: T) -> int:
+        if item in self.__bag:
+            count = self.__bag[item]
+            print("There are" + count + item + "s in your bag")
+        else:
+            while not(item in self.__bag):
+                print("There are no items of that type in your bag")
+                self.__bag.count()
         raise NotImplementedError("count method not implemented")
 
     def __len__(self) -> int:
         raise NotImplementedError("__len__ method not implemented")
 
-    def distinct_items(self) -> int:
+    def distinct_items(self) -> Iterable[T]:
         raise NotImplementedError("distinct_items method not implemented")
 
     def __contains__(self, item) -> bool:
