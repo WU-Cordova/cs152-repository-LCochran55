462a504d8dbab51bfe542c088bb9533f25a4dbcf
LCochran55 <liamcochran55@gmail.com>
2025-02-05 12:29:23 -0800

    commit

--- projects/project1/program.py
+++ projects/project1/program.py
@@ -1,10 +1,17 @@
 
-
-
+from datastructures.bag import Bag
+from projects.project1.card import Card,CardSuit,CardFace
+from projects.project1.multideck import Multideck
+from projects.project1.game import Game
 
 def main():
-    
-    print("Hello, World!")
+
+    player = Bag
+    dealer = Bag
+    deck = Multideck()
+    game = Game(player,dealer,deck)
+    game.deal()
+
 
 
 
