c55aa3f5736405003eb41c6cb6f0e99f2b59bf5a
LCochran55 <liamcochran55@gmail.com>
2025-02-14 16:25:26 -0800

    commit

--- datastructures/array.py
+++ datastructures/array.py
@@ -11,6 +11,7 @@ import os
 from typing import Any, Iterator, overload
 import numpy as np
 from numpy.typing import NDArray
+import copy
 
 
 from datastructures.iarray import IArray, T
@@ -20,8 +21,13 @@ class Array(IArray[T]):
 
     def __init__(self, starting_sequence: Sequence[T]=[], data_type: type=object) -> None: 
         self.__logical_size: int = len(starting_sequence)
-        self.__physics_size: int = self.__logical_size
+        self.__physical_size: int = self.__logical_size
         self.__data_type: type = data_type
+
+        self.__elements = np.empty(self.__logical_size, dtype=self.__data_type)
+
+        for index in range(self.__logical_size):
+            self.__items[index] = copy.deepcopy(starting_sequence[index])
 
         if not isinstance(starting_sequence,Sequence):
             raise ValueError("starting_sequence must be a valid sequence type")
@@ -34,11 +40,6 @@ class Array(IArray[T]):
             if not isinstance(item,self.__data_type):
                 raise TypeError(f"Item{repr(item)} is not of type {str(data_type)}")
 
-        self.__items: NDArray = np.empty(self.__logical_size,dtype:self.__data_type)
-
-        for index in range(self.__logical_size):
-            self.__items[index] = starting_sequence[index]
-
 
 
     @overload
@@ -46,18 +47,57 @@ class Array(IArray[T]):
     @overload
     def __getitem__(self, index: slice) -> Sequence[T]: ...
     def __getitem__(self, index: int | slice) -> T | Sequence[T]:
+            #make sure item exists 
+            if isinstance(index,slice):
+                start = slice.start
+                stop = slice.stop
+                step = slice.step
+
+                if start<self.__logical_size-1 or start>-(self.__logical_size-1) and stop <= self.__logical_size-1 or stop >= -(self.__logical_size-1):
+                    return Array(starting_sequence=self.__elements[index].tolist(),data_type=self.__data_type) # item if its a slice
+                else:
+                    raise IndexError("Your start or stop if out of range of your list")
+                #check if start abd sto[ are in nobund]
+                #if nto raise an exception  
+            
+            elif isinstance(index,int):
+                if(index<=self.__logical_size-1):
+                    return self.__elements[index] #item if index is an int
+                else:
+                    raise IndexError("Index is out of range")
+                #check if index in bounds
+                #if not raise an exception
+
+
             raise NotImplementedError('Indexing not implemented.')
     
     def __setitem__(self, index: int, item: T) -> None:
+        #check if index if out of bounds or iem is right type
+        self.__elements[index] = item
         raise NotImplementedError('Indexing not implemented.')
 
     def append(self, data: T) -> None:
+
         raise NotImplementedError('Append not implemented.')
+    
+    def __grow(self,new_size:int) -> None:
+        #for append methods
+
+        #If array doesnt need to grow, exit
+        #Create a new numpy array with new size
+        #copy over the elementsfrom the crrent numpy array to the new one
+        #set the current numpy array to the new one
+        pass
 
     def append_front(self, data: T) -> None:
+        #append item tofront of list
+        #Groaw as needed
         raise NotImplementedError('Append front not implemented.')
 
     def pop(self) -> None:
+        #self.__delitem__(self.__logical_size+-count-1)
+        #or
+        #         #del self[]
         raise NotImplementedError('Pop not implemented.')
     
     def pop_front(self) -> None:
@@ -79,6 +119,8 @@ class Array(IArray[T]):
         raise
 
     def __delitem__(self, index: int) -> None:
+       #deletes item at the index\
+       # .// capacity (floor)
        raise NotImplementedError('Delete not implemented.')
 
     def __contains__(self, item: Any) -> bool:
@@ -87,6 +129,7 @@ class Array(IArray[T]):
         raise NotImplementedError('Contains not implemented.')
 
     def clear(self) -> None:
+        #make new empty array  and reassign
         raise NotImplementedError('Clear not implemented.')
 
     def __str__(self) -> str:
